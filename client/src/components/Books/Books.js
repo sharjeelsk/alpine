89ec771c07a27e1dc432cf067bd9360a6ea282fa
sharjeel shaikh <sharjeelarts@gmail.com>
2021-07-13 15:43:54 +0530

    added

--- client/src/components/Books/Books.js
+++ client/src/components/Books/Books.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Books.scss"
 import Header from '../Header/Header'
 import SectionCard from '../UiKits/SectionCard'
-import stationaries from '../Assets/books (1).json'
+import stationaries from '../Assets/books (2).json'
 import Footer from '../Footer/Footer'
 const Books = ({history}) => {
     let entries  = Object.entries(stationaries)
